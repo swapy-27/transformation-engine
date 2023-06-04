d1854858b7c7184cbb7c75ce17703740f13fe850
swapy <swapniljain240@gmail.com>
2023-06-04 16:11:33 +0530

    springDataJpa learning

--- src/main/java/com/project/integrator_engine/config/JmsConfig.java
+++ src/main/java/com/project/integrator_engine/config/JmsConfig.java
@@ -1,26 +1,26 @@
-package com.project.integrator_engine.config;
-
-import jakarta.jms.ConnectionFactory;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-
-@Configuration
-@EnableJms
-public class JmsConfig {
-    @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer){
-
-        DefaultJmsListenerContainerFactory defaultJmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
-
-        defaultJmsListenerContainerFactory.setConnectionFactory(connectionFactory);
-        defaultJmsListenerContainerFactory.setConcurrency("5-10");
-
-        configurer.configure(defaultJmsListenerContainerFactory,connectionFactory);
-
-        return  defaultJmsListenerContainerFactory;
-
-    }
-}
+//package com.project.integrator_engine.config;
+//
+//import jakarta.jms.ConnectionFactory;
+//import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jms.annotation.EnableJms;
+//import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+//
+//@Configuration
+//@EnableJms
+//public class JmsConfig {
+//    @Bean
+//    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer){
+//
+//        DefaultJmsListenerContainerFactory defaultJmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
+//
+//        defaultJmsListenerContainerFactory.setConnectionFactory(connectionFactory);
+//        defaultJmsListenerContainerFactory.setConcurrency("5-10");
+//
+//        configurer.configure(defaultJmsListenerContainerFactory,connectionFactory);
+//
+//        return  defaultJmsListenerContainerFactory;
+//
+//    }
+//}
